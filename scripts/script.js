88947e75e6bfb7b3466819b9852e92935412b73e
Vladi Leshko <leshkovladik@gmail.com>
2022-08-29 15:16:14 +0300

    finished questionary page and modal windows

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,32 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 };
+
+//modal window script
+var modal = document.getElementById("modalWindow");
+var firstBtn = document.getElementById("firstModalButton");
+var secondBtn = document.getElementById("secondModalButton");
+var thirdBtn = document.getElementById("thirdModalButton");
+var span = document.getElementsByClassName("closeButton")[0];
+
+firstBtn.onclick = function() {
+    modal.style.display = "block";
+};
+
+secondBtn.onclick = function() {
+    modal.style.display = "block";
+};
+
+thirdBtn.onclick = function() {
+    modal.style.display = "block";
+};
+
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+};
