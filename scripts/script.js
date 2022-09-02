c035dd27ddfbb850e159f60c3acd622161efa8e2
Vladik Leshko <leshkovladik@gmail.com>
2022-09-02 14:41:50 +0300

    added slider to another pages and some fixes

--- scripts/script.js
+++ scripts/script.js
@@ -20,6 +20,33 @@ function menuOnClick() {
     document.getElementById("menu-bg").classList.toggle("change-bg");
 };
 
+//slider script
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
 //modal window script
 var modal = document.getElementById("modalWindow");
 var firstBtn = document.getElementById("firstModalButton");
@@ -55,30 +82,3 @@ window.onclick = function(event) {
       modal.style.display = "none";
     }
 };
-
-//slider script
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-}
