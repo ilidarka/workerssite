d61270f31503fdb245c4d1cebd02bf87c91b5510
Vladik Leshko <leshkovladik@gmail.com>
2022-09-06 14:14:45 +0300

    added forms on index

--- mail.php
+++ mail.php
@@ -11,10 +11,10 @@ $text = $_POST['user_text'];
 $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'workerssite@yandex.by'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '123456789zxcASDqwe'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Password = 'gjferovipjkkfeif'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
